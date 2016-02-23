964bf9b1083043b9d641e7e9f0f2103ace27ca3c
Tim Knight <timmilesknight@gmail.com>
2016-02-23 15:03:30 +0000

    cross origin request fix

--- server.js
+++ server.js
@@ -18,6 +18,15 @@ var port = process.env.PORT || 3000; // set our port
 // Get routes
 var router = require('./routes/palettes');
 
+
+// Configure app to handle CORS requests
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+  next();
+});
+
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
