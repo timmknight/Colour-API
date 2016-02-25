1b37b6e93b8b1557ab454d80120225be978f14d2
Tim Knight <timmilesknight@gmail.com>
2016-02-25 15:55:16 +0000

    allow post requests

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var router = require('./routes/palettes');
 // Configure app to handle CORS requests
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
   next();
 });
